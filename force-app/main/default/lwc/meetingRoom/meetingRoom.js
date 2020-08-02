c5400f9188917c4f53c564094efbf8ebbd6293f3
Max Sh Ubuntu <maxshev2000@gmail.com>
2020-08-02 08:47:20 +0300

    meetingRoom current changes

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import { LightningElement, api} from 'lwc';
 
 export default class MeetingRoom extends LightningElement {
     @api meetingRoomInfo; // {roomName:'A-001', roomCapacity:'12'}
 }
-=======
-import { LightningElement } from 'lwc';
-
-export default class MeetingRoom extends LightningElement {}
->>>>>>> 304d1902731e5bd7d7142a9f1d2a998fff196818
